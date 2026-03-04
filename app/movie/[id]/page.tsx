78bf47c195843997730cd99bcb1a0a205f2245a6
Nizam <nizam.frontend@gmail.com>
2026-03-04 15:43:09 +0530

    fix: replace axios with fetch for TMDB reviews API

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,27 +1,32 @@
-import { fetchMovieById } from "@/lib/omdb"
-import MovieDetails from "@/components/MovieDetails"
-import type { Movie } from "@/types/movie"
+import { fetchMovieById } from "@/lib/omdb";
+import MovieDetails from "@/components/MovieDetails";
+import type { Movie } from "@/types/movie";
+import { fetchMovieReviews } from "@/lib/reviews";
 
 interface Props {
   params: Promise<{
-    id: string
-  }>
+    id: string;
+  }>;
 }
 
-export default async function MoviePage({ params }: Props){
+export default async function MoviePage({ params }: Props) {
+  const { id } = await params;
 
-  const { id } = await params
+  const movie: Movie = await fetchMovieById(id);
+  const reviews = await fetchMovieReviews(movie.Title);
 
-  const movie: Movie = await fetchMovieById(id)
-
-  return(
-
+  return (
     <div className="min-h-screen flex justify-center items-center p-8">
-
       <MovieDetails movie={movie} />
+      <div className="mt-10 max-w-xl">
+        <h2 className="text-2xl font-semibold mb-4">Audience Reviews</h2>
 
+        {reviews.slice(0, 3).map((review: any, index: number) => (
+          <p key={index} className="text-gray-300 mb-3">
+            {review.content}
+          </p>
+        ))}
+      </div>
     </div>
-
-  )
-
+  );
 }
--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,41 +1,30 @@
-import axios from "axios"
-
 const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY
 
 export const fetchMovieReviews = async (movieTitle: string) => {
 
-  try{
+  try {
 
-    // search movie in TMDB
-    const searchRes = await axios.get(
-      "https://api.themoviedb.org/3/search/movie",
-      {
-        params:{
-          api_key: TMDB_API_KEY,
-          query: movieTitle
-        }
-      }
+    const searchRes = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(movieTitle)}`, 
     )
 
-    const movieId = searchRes.data.results[0]?.id
+    const searchData = await searchRes.json()
 
-    if(!movieId) return []
+    const movieId = searchData?.results?.[0]?.id
 
-    // fetch reviews
-    const reviewRes = await axios.get(
-      `https://api.themoviedb.org/3/movie/${movieId}/reviews`,
-      {
-        params:{
-          api_key: TMDB_API_KEY
-        }
-      }
+    if (!movieId) return []
+
+    const reviewRes = await fetch(
+     `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${TMDB_API_KEY}`,
     )
 
-    return reviewRes.data.results
+    const reviewData = await reviewRes.json()
 
-  }catch(error){
+    return reviewData?.results || []
 
-    console.error("Error fetching reviews:",error)
+  } catch (error) {
+
+    console.error("TMDB fetch error:", error)
 
     return []
 
